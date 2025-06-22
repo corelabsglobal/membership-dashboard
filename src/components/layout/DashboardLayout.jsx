9105364f3d069d902a955602ec1613cb477f2306
kwamebaah1 <baahhprince79@gmail.com>
2025-06-22 23:38:18 +0000

    Mobile Navigation

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -1,15 +1,32 @@
+"use client"
+
+import { useState } from 'react'
 import { Sidebar } from './Sidebar'
 import { Topbar } from './Topbar'
 
 export function DashboardLayout({ children }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen)
+  }
+
   return (
     <div className="flex min-h-screen bg-background">
-      <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
+      {isSidebarOpen && (
+        <div 
+          className="fixed inset-0 z-20 bg-black/50 md:hidden" 
+          onClick={toggleSidebar}
+        />
+      )}
+      
+      {/* Sidebar */}
+      <div className={`fixed inset-y-0 z-30 w-64 transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
         <Sidebar />
       </div>
       
       <div className="flex flex-col flex-1 md:pl-64">
-        <Topbar />
+        <Topbar toggleSidebar={toggleSidebar} />
         
         <main className="flex-1 p-4 md:p-6">
           <div className="mx-auto max-w-7xl">
