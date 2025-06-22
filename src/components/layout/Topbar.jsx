5899fbdf75bbaf0cdacb0b65080fbb51f9c04800
kwamebaah1 <baahhprince79@gmail.com>
2025-06-22 23:38:35 +0000

    Mobile Navigation

--- src/components/layout/Topbar.jsx
+++ src/components/layout/Topbar.jsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { Bell, Search, User } from 'lucide-react'
+import { Bell, Search, User, Menu } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { useRouter } from 'next/navigation'
 import { supabase } from '@/lib/supabase'
 import { useEffect, useState } from 'react'
 
-export function Topbar() {
+export function Topbar({ toggleSidebar }) {
   const router = useRouter()
   const [isAuthenticated, setIsAuthenticated] = useState(false)
 
@@ -23,13 +23,23 @@ export function Topbar() {
   return (
     <header className="sticky top-0 z-10 bg-card shadow-sm">
       <div className="flex items-center justify-between h-16 px-6">
+        <div className="flex items-center md:hidden">
+          <Button 
+            variant="ghost" 
+            size="icon" 
+            className="rounded-full cursor-pointer"
+            onClick={toggleSidebar}
+          >
+            <Menu className="w-5 h-5" />
+          </Button>
+        </div>
         <div className="flex items-center flex-1 max-w-md">
-          <Search className="w-5 h-5 ml-3 text-muted-foreground absolute" />
+          {/*<Search className="w-5 h-5 ml-3 text-muted-foreground absolute" />
           <Input
             type="search"
             placeholder="Search..."
             className="pl-10 bg-muted/50 border-none focus-visible:ring-1"
-          />
+          />*/}
         </div>
         <div className="flex items-center space-x-4">
           <Button variant="ghost" size="icon" className="rounded-full cursor-pointer">
